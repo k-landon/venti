26efdeeefb0dfe49473f2e7c665b00a4564ccfa0
Kate Landon <a1767709@student.adelaide.edu.au>
2022-01-02 23:11:09 +1030

    Added database connection

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 // Require the necessary discord.js classes
+const { connect } = require('mongoose');
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 
-dotenv.config();
+(async () => {
 
+dotenv.config();
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.once('ready', () =>
+	{
+		console.log('Ready!');
+	}
+);
+
+// Connect to Mongodb database
+await connect(process.env.MONGODB_URI).then(
+	() =>
+	{
+		console.log("Database Connected!");
+	}
+);
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
+})();
